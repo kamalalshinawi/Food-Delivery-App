053543593978566f94acb250fff4bde8b32cfb50
Kamalalshinawi <kamalalshinawi@gmail.com>
2026-04-21 15:00:57 +0200

    feat: build banner View

--- src/components/BannerView.tsx
+++ src/components/BannerView.tsx
@@ -1,14 +1,34 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, TouchableOpacity, ImageBackground } from 'react-native';
+import React from 'react';
+import { s, vs } from 'react-native-size-matters';
 
 const BannerView = () => {
   return (
-    <View>
-      <Text>BannerView</Text>
-    </View>
-  )
-}
+    <TouchableOpacity style={styles.container}>
+      <ImageBackground
+        source={require('../assets/images/banner1.png')}
+        style={styles.ImageBanner}
+        resizeMode="contain"
+      />
+    </TouchableOpacity>
+  );
+};
 
-export default BannerView
+export default BannerView;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    height: vs(200),
+    width: s(330),
+    borderRadius: s(20),
+    backgroundColor: 'red',
+    marginVertical: vs(20),
+    alignSelf: 'center',
+    overflow: 'hidden',
+  },
+  ImageBanner: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+  },
+});
